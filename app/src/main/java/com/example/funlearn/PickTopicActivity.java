88f23426e2deccded1eadb637c31ef5094deb0c4
yamlakyam <yamlakyam@gmail.com>
2021-12-03 10:34:45 +0300

    added an empty pick topic actiivty

---
+++ app/src/main/java/com/example/funlearn/PickTopicActivity.java
@@ -0,0 +1,14 @@
+package com.example.funlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickTopicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_topic);
+    }
+}
