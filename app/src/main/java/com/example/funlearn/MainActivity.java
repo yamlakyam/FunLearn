806d6434ccfc2277d7e54740508dbcbaa72ca20a
yamlakyam <yamlakyam@gmail.com>
2021-12-07 15:15:15 +0300

    started the home fragment page design

--- app/src/main/java/com/example/funlearn/MainActivity.java
+++ app/src/main/java/com/example/funlearn/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.funlearn;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.funlearn.Util.PreferenceKeys;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resetPrefernce();
+        isFirstLogin();
+
     }
+
+    @Override
+    public void onClick(View v) {
+        //    startActivity();
+//    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    public void isFirstLogin() {
+
+//        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstLogin = preferences.getBoolean(PreferenceKeys.FIRST_LOGIN, true);
+
+        if (isFirstLogin) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("We have noticed this is your first login. Make sure to select your preferred topics in the setting screen");
+            alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("TAG", "onClick: closing dialog");
+
+                    editor = preferences.edit();
+                    editor.putBoolean(PreferenceKeys.FIRST_LOGIN, false);
+                    editor.commit();
+                    dialog.dismiss();
+                }
+            });
+            alertDialogBuilder.setIcon(R.drawable.briefcase);
+            alertDialogBuilder.setTitle(" ");
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+        }
+
+    }
+
+    public void resetPrefernce() {
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstLogin = preferences.getBoolean(PreferenceKeys.FIRST_LOGIN, true);
+        editor = preferences.edit();
+        editor.putBoolean(PreferenceKeys.FIRST_LOGIN, true);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/funlearn/Util/PreferenceKeys.java
@@ -0,0 +1,6 @@
+package com.example.funlearn.Util;
+
+public class PreferenceKeys {
+    public static final String FIRST_LOGIN="first_login";
+    public static final String TOPIC_CHOSEN ="topic";
+}
