c4730a1c9d6d92cd10a9ad0b5553f4e6d15af89d
yamlakyam <yamlakyam@gmail.com>
2021-12-10 14:12:36 +0300

    parsing done

--- app/src/main/java/com/example/funlearn/HomeFragment.java
+++ app/src/main/java/com/example/funlearn/HomeFragment.java
@@ -16,6 +16,7 @@ import com.example.funlearn.Retrofit.ApiInterface;
 import com.example.funlearn.Retrofit.BasicAuthInterceptor;
 import com.example.funlearn.Retrofit.RetrofitClientInstance;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -27,8 +28,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
 
-//    private static final String BASE_URL = "https://www.udemy.com/api-2.0/courses";
+    //    private static final String BASE_URL = "https://www.udemy.com/api-2.0/courses";
     private static final String BASE_URL = "https://www.udemy.com/api-2.0/";
+
+    public static ArrayList<CourseInfo> courseInfoArrayList;
 
 
     @Override
@@ -36,8 +39,8 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        String username="KWr09PPO6cuarAQ23r22fUCTMTJluKIpkheoN3eE";
-        String password="55iS1BiEsxIfx3osg9MXlHhKW1AOhzWaXcNbAjzJDssqFtYQpYxAwZ4xvHqXZ94PgbdDVEjIsMXjL7NHh0y0pWdweTvTzE9UhkSA04nS4UCdsmiuSqDruR0ATUaKVI9J";
+        String username = "KWr09PPO6cuarAQ23r22fUCTMTJluKIpkheoN3eE";
+        String password = "55iS1BiEsxIfx3osg9MXlHhKW1AOhzWaXcNbAjzJDssqFtYQpYxAwZ4xvHqXZ94PgbdDVEjIsMXjL7NHh0y0pWdweTvTzE9UhkSA04nS4UCdsmiuSqDruR0ATUaKVI9J";
 
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new BasicAuthInterceptor(username, password)).build();
 
@@ -51,9 +54,14 @@ public class HomeFragment extends Fragment {
         call.enqueue(new Callback<AllUdemyData>() {
             @Override
             public void onResponse(Call<AllUdemyData> call, Response<AllUdemyData> response) {
-                Log.i("onResponse", response.body().toString());
-            }
+                Log.i("onResponse", response.body().getCourseInfoArrayList().toString());
 
+                courseInfoArrayList = new ArrayList<>();
+                courseInfoArrayList.addAll(response.body().getCourseInfoArrayList());
+
+                Log.i("All courses", courseInfoArrayList.get(0).getHeadline() + "");
+
+            }
 
 
             @Override
